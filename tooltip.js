4c772d00918d58b7f76a10eef2c1b8cfab3810e7
Alex O <organ.a.f@gmail.com>
2016-05-08 19:30:25 +0300

    Fixed animation for toggle popover; Added code documentation;

--- tooltip.js
+++ tooltip.js
@@ -2,51 +2,52 @@
  * This small library allows to add tooltips to page elements.
  * @author Alex O. <organ.f.a@gmail.com>
  */
- 
+
 /* global $ */
 
-// (function($, Tooltips){
+(function ($, ttip) {
 
     /**
      * Debounce function calls
-     * 
+     *
      * @param {Function} fn
      * @param {Number} delay Timeout delay in milliseconds
-     */ 
+     */
     function debounce(fn, delay) {
-      var timer = null;
-      return function () {
-        var context = this, args = arguments;
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-          fn.apply(context, args);
-        }, delay);
-      };
+        var timer = null;
+        return function () {
+            var context = this, args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                fn.apply(context, args);
+            }, delay);
+        };
     }
 
 
     // Debug parameters
-    var DEBUG = true;
+    var DEBUG = false;
 
     /**
      * Pring rebug message if debugging enabled
      * @param {String} message
      */
-    function debug(message){
+    function debug(message) {
         if (DEBUG) console.log(message);
     }
-    
+
     /**
      * Bounding box class
      * @param {HTMLElement} element
      * @return {object} Element dimensions
      * @constructor
      */
-    function BBox(element){
+    function BBox(element) {
         var self = this
-        ,   W = element.offsetParent.offsetWidth
-        ,   H = element.offsetParent.offsetHeight
-        ;
+            , W = element.offsetParent.offsetWidth
+            , H = element.offsetParent.offsetHeight
+            ;
+
         /**
          * Element dimensions object
          * @attr {int} top Available space to the top
@@ -56,29 +57,29 @@
          * @attr {object} center Coordinates of cenver of tooltipped element
          */
         var D = {
-            get top(){
+            get top() {
                 return element.offsetTop;
             },
-            get left(){
+            get left() {
                 return element.offsetLeft;
             },
-            get right(){
-                return W 
-                    - this.left 
+            get right() {
+                return W
+                    - this.left
                     - element.offsetWidth
-                ;
+                    ;
             },
-            get bottom(){
+            get bottom() {
                 return H
-                     - this.top
-                     - element.offsetHeight
-                ;
+                    - this.top
+                    - element.offsetHeight
+                    ;
             },
             center: {
-                get x(){
+                get x() {
                     return D.left + element.offsetWidth / 2;
                 },
-                get y(){
+                get y() {
                     return D.top + element.offsetHeight / 2;
                 }
             },
@@ -87,52 +88,52 @@
         };
         return D;
     }
-    
+
     /**
-     * Base class for tooltips and popovers
-     * @memberOf Toottips
+     * Base class for tooltips and popovers. Tooltip and popover elements add new div element inside
+     * target. This div element will be shown when ucrsor is over tooltip target.
+     *
+     * @constructor
+     * @memberOf ttip
      * @param {jQuery} target
+     * @param {string} type
      * @type {HTMLDivElement} this.target
      */
-    function BaseTooltip(target, type, displayDelay){
+    function BaseTooltip(target, type) {
         this.$target = target;
-        this.displayDelay = displayDelay || 0;
         this.target = this.$target.get(0);
         this.type = type;
         this.PADDING = 10;
         this.POSITIONS = [this.TOP, this.BOTTOM, this.RIGHT, this.LEFT];
-        
+
         this.bbox = new BBox(this.target);
         this.container = this.addContainer();
-        setTimeout(function(){
+        setTimeout(function () {
             this.render();
-        }.bind(this), this.displayDelay);
+        }.bind(this), 0);
     }
-    
-    BaseTooltip.prototype.test = function(){
-        debug(this.type);
-    };
-    
-    // BaseTooltip.prototype.setContent = function(container){
-    //     container.text(this.$target.attr('data-tooltip'));
-    // };
-    
-    BaseTooltip.prototype.addContainer = function(){
+
+    /**
+     * Add tooltip container (div element) to target
+     *
+     * @function
+     * @memberOf BaseTooltip
+     */
+    BaseTooltip.prototype.addContainer = function () {
         var before = $('<div/>').addClass('before')
-        ,   after = $('<div/>').addClass('after')
-        ,   containerWidth = 250
-        ,   pos = this.getPosition()
-        ,   minWidth = 50
-        ,   avail = this.bbox[pos]
-        // ,   content = this.$target.attr('data-tooltip')
-        ;
-        if (pos == this.LEFT || pos == this.RIGHT){
-            if (avail < minWidth){
+            , after = $('<div/>').addClass('after')
+            , containerWidth = 250
+            , pos = this.getPosition()
+            , minWidth = 50
+            , avail = this.bbox[pos]
+            ;
+        if (pos == this.LEFT || pos == this.RIGHT) {
+            if (avail < minWidth) {
                 containerWidth = minWidth;
-            } else if (avail < containerWidth){
+            } else if (avail < containerWidth) {
                 containerWidth = avail - this.PADDING * 2;
             }
-        } 
+        }
         this.pin = before.add(after);
         this.container = $('<div/>')
             .css({
@@ -147,74 +148,81 @@
         ;
         var contentEl = $('<div/>')
             .addClass('ttip-content')
-            // .text(content)
             .appendTo(this.container)
-        ;
+            ;
         this.setContent(contentEl);
         this.container.width(contentEl.width() + this.PADDING * 2);
-        setTimeout(function(){
+        setTimeout(function () {
             this.container.css({
                 visibility: '',
                 opacity: ''
             });
-            // this.container.removeAttr('style');
-        }.bind(this), (this.displayDelay||0));
+        }.bind(this), 0);
         return this.container;
     };
-    
+
     /**
      * Get available space to each side of the tooltip target
-     * @return {Array<Number>} Array of available space to the top, bottom, 
+     *
+     * @function
+     * @memberOf BaseTooltip
+     * @return {Array<Number>} Array of available space to the top, bottom,
      *      right, left
      */
-    BaseTooltip.prototype.getAvail = function(pos){
-        return this.POSITIONS.map(function(p){
-          return this.bbox[p];
+    BaseTooltip.prototype.getAvail = function () {
+        return this.POSITIONS.map(function (p) {
+            return this.bbox[p];
         }.bind(this));
     };
-    
-    
-    BaseTooltip.prototype.getPosition = function(){
+
+
+    /**
+     * Calculate ttip position from `data-ttip-position` or where's most available space
+     *
+     * @returns {string} Calculated position
+     */
+    BaseTooltip.prototype.getPosition = function () {
         var positions = [this.TOP, this.BOTTOM, this.RIGHT, this.LEFT]
-        ,   attr = this.target.getAttribute('data-ttip-position')
-        ;
-        if (attr){
-            if (positions.indexOf(attr) != -1){
+            , attr = this.target.getAttribute('data-ttip-position')
+            ;
+        if (attr) {
+            if (positions.indexOf(attr) != -1) {
                 return attr;
             } else {
                 throw Error("Incorrect value of \"ttip-position\" attr: " + attr);
             }
         } else {
             var avail = this.getAvail();
-            var maxAvail =  Math.max.apply(null, avail);
-            debug(maxAvail);
-            debug(avail.indexOf(maxAvail));
+            var maxAvail = Math.max.apply(null, avail);
             return positions[avail.indexOf(maxAvail)];
         }
     };
-    
-    /**
-     * Remove tooltip contatiner from element
-     */
-    BaseTooltip.prototype.reset = function(){
-        $('.ttip-container').remove();
-    };
-    
 
     /**
-     * 
+     * Remove tooltip container from element
+     * @memberOf BaseTooltip
      */
-    BaseTooltip.prototype.render = function(){
+    BaseTooltip.prototype.reset = function () {
+        $('.ttip-container').remove();
+    };
+
+
+    /**
+     * Set container position
+     * @memberOf BaseTooltip
+     */
+    BaseTooltip.prototype.render = function () {
         var $c = this.container instanceof $ ? this.container : $(this.container)
-        ,   c = $c.get(0)
-        ,   shiftx =(this.bbox.w - c.offsetWidth) / 2
-        ,   shifty = (this.bbox.h - c.offsetHeight) / 2
-        ,   position = this.getPosition()
-        ,   minpad = 5
-        ;
+            , c = $c.get(0)
+            , shiftx = (this.bbox.w - c.offsetWidth) / 2
+            , shifty = (this.bbox.h - c.offsetHeight) / 2
+            , position = this.getPosition()
+            , minpad = 5
+            , pinshift
+            ;
         debug('Container shift x: ' + this.bbox.w + ' - ' + c.offsetWidth + ' / 2 = ' + shiftx);
         debug('Container shift y: ' + this.bbox.h + ' - ' + c.offsetHeight + ' / 2 = ' + shifty);
-        switch (position){
+        switch (position) {
             case this.TOP:
                 shifty = -(c.offsetHeight + this.PADDING);
                 break;
@@ -227,47 +235,47 @@
             case this.LEFT:
                 shiftx = -(c.offsetWidth + this.PADDING);
                 break;
-            default: 
+            default:
                 throw Error("Unknown position: " + position);
         }
-        
+
         // Element position correction to fit in on the page
-        if (position == this.TOP || position == this.BOTTOM){
+        if (position == this.TOP || position == this.BOTTOM) {
             // Horizontal correction
-            var c = this.container instanceof $ ? this.container.get(0) : this.container
-            ,   cbbox = c.getBoundingClientRect()
-            ;
-            var fitsLeft = (this.bbox.w/2 + this.bbox.left + minpad*2) > c.offsetWidth/2;
-            var fitsRight = (this.bbox.w/2 + this.bbox.right + minpad*2) > c.offsetWidth/2;
-            if (!fitsLeft || !fitsRight){
-                if (!fitsLeft){
+            var c = this.container instanceof $ ? this.container.get(0) : this.container;
+            var fitsLeft = (this.bbox.w / 2 + this.bbox.left + minpad * 2) > c.offsetWidth / 2;
+            var fitsRight = (this.bbox.w / 2 + this.bbox.right + minpad * 2) > c.offsetWidth / 2;
+            if (!fitsLeft || !fitsRight) {
+                if (!fitsLeft) {
                     shiftx = minpad - this.bbox.left;
-                    var pinshift = this.bbox.center.x - minpad;
+                    pinshift = this.bbox.center.x - minpad;
                     this.pin.css('left', pinshift);
                 } else if (!fitsRight) {
                     shiftx = this.bbox.right - minpad + this.bbox.w - c.offsetWidth;
-                    var pinshift = this.bbox.w/2 + this.bbox.right - minpad - 10;
+                    pinshift = this.bbox.w / 2 + this.bbox.right - minpad - 10;
                     this.pin.css('right', pinshift).css('left', 'initial');
                 }
             }
         } else {
-            var fitsTop = (this.bbox.h/2 + this.bbox.top + minpad*2) > c.offsetHeight/2;
-            var fitsBottom = (this.bbox.h/2 + this.bbox.bottom + minpad*2) > c.offsetHeight/2;
+            var fitsTop = (this.bbox.h / 2 + this.bbox.top + minpad * 2) > c.offsetHeight / 2;
+            var fitsBottom = (this.bbox.h / 2 + this.bbox.bottom + minpad * 2) > c.offsetHeight / 2;
             // Verticall correction
-            if (!fitsBottom){
+            if (!fitsBottom) {
                 shifty = this.bbox.bottom - minpad + this.bbox.h - c.offsetHeight;
-                var pinshift = this.bbox.h/2 + this.bbox.bottom - minpad - 10;
+                pinshift = this.bbox.h / 2 + this.bbox.bottom - minpad - 10;
                 this.pin.css('bottom', pinshift).css('top', 'initial');
             } else if (!fitsTop) {
                 shifty = minpad - this.bbox.top;
-                var pinshift = this.bbox.center.y - minpad;
+                pinshift = this.bbox.center.y - minpad;
                 this.pin.css('top', pinshift);
             }
         }
-        
+
         $c.addClass(position).css('top', shifty).css('left', shiftx);
         return this.container;
     };
+
+    // Helper properties
 
     BaseTooltip.prototype.TOOLTIP = 'tooltip';
     BaseTooltip.prototype.POPOVER = 'popover';
@@ -275,96 +283,113 @@
     BaseTooltip.prototype.BOTTOM = 'bottom';
     BaseTooltip.prototype.RIGHT = 'right';
     BaseTooltip.prototype.LEFT = 'left';
-    
-    
+
+
     /**
-     * Tooltip
+     * Tooltip class
+     *
+     * @constructor
+     * @extends BaseTooltip
+     * @param {jQuery} target Target element
      */
-    function Tooltip(target){
+    function Tooltip(target) {
         this.base = BaseTooltip;
         this.base(target, this.TOOLTIP);
     }
-    
+
     Tooltip.prototype = Object.create(BaseTooltip.prototype);
-    
-    Tooltip.prototype.setContent = function(container){
+
+    /**
+     * Add content to ttip container element from tooltip target attribute
+     *
+     * @function
+     * @memberOf Tooltip
+     * @param {jQuery} container
+     */
+    Tooltip.prototype.setContent = function (container) {
         var text = this.$target.attr('data-tooltip');
         debug("Add tooltip text content: " + text.length + ' characters.');
         container.text(text);
     };
-    
+
     /**
-     * Popover
+     * Popover class
+     *
+     * @constructor
+     * @extends BaseTooltip
+     * @param {jQuery} target Target element
      */
-    function Popover(target){
+    function Popover(target) {
         this.base = BaseTooltip;
-        this.base(target, this.POPOVER, 500);
+        this.base(target, this.POPOVER);
     }
-    
+
     Popover.prototype = Object.create(BaseTooltip.prototype);
-    
-    Popover.prototype.setContent = function(container){
+
+    /**
+     * Add content to ttip container element by id of html element which will be used as template
+     *
+     * @function
+     * @memberOf Tooltip
+     * @param {jQuery} container
+     */
+    Popover.prototype.setContent = function (container) {
         var content = this.$target.attr('data-popover');
         debug("Add popover content from " + content);
         container.html($('[data-popover-content=' + content + ']').html());
     };
 
-    $(document).on('ready', function(){
-        // setTimeout(function(){
-        //     // $('[data-tooltip]').each(function(i,e){
-        //     //     t = new Tooltip($(e));
-        //     // });
-        //     // t7 = new Tooltip($('[data-tooltip]').eq(7));
-        //     // t8 = new Tooltip($('[data-tooltip]').eq(8));
-        //     // t2 = new Tooltip($('[data-tooltip]').eq(2));
-        //     // t3 = new Tooltip($('[data-tooltip]').last());
-        // }, 300);
-        
-        $(document).on('mouseenter', '[data-tooltip]', function(e){
+    $(document).on('ready', function () {
+
+        // Create and show tooltip element
+        $(document).on('mouseenter', '[data-tooltip]', function (e) {
             var target = e.target
-            ,   $target = $(target)
-            ;
-            if (!$target.has('.ttip-container').length){
+                , $target = $(target)
+                ;
+            if (!$target.has('.ttip-container').length) {
                 new Tooltip($target);
             }
         });
-        
-        $(document).on('mouseenter', '[data-popover]', function(e){
+
+        // Create and show popover element
+        $(document).on('mouseenter', '[data-popover]', function (e) {
             var target = e.target
-            ,   $target = $(target)
-            ;
-            if (!$target.has('.ttip-container').length && $target.is('[data-popover]')){
-                console.log("New popover");
-                console.log($target);
+                , $target = $(target)
+                ;
+            if (!$target.has('.ttip-container').length && $target.is('[data-popover]')) {
                 new Popover($target);
             }
         });
-        
-        $(document).on('click', '[data-popover].toggle', function(e){
+
+        // Toggle popover open
+        $(document).on('click', '[data-popover].toggle', function (e) {
             var $t = $(e.target);
-            if ($t.hasClass('open')){
+            if ($t.hasClass('open')) {
                 $t.removeClass('open');
             } else {
                 $t.addClass('open');
             }
         });
-        
+
+        $(document).on('dblclick', '[data-popover].toggle', function(e){
+            e.preventDefault();
+            e.stopPropagation();
+        });
+
     });
-    
-    
-    
-    
-    
+
+
     /**
-     * Toottips namespace.
+     * Tooltips namespace.
      * @namespace
      */
-    Tooltips = {
-        
-    }
-    
-    $(window).resize(debounce(function(e){
+    ttip = {
+        Tooltip: Tooltip,
+        Popover: Popover
+    };
+
+    $(window).resize(debounce(function (e) {
         BaseTooltip.prototype.reset();
     }, 200));
 
-// })(jQuery, window.Tooltips);
+})(jQuery, window.ttip);
